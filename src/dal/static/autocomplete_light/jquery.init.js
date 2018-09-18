53755cf766fa22d35a8e1ca83a3152ac5e1ed614
Gianni Moschini <gianni.moschini+github@gmail.com>
2018-09-18 12:14:12 +0100

    Init jquery from django.jQuery

--- src/dal/static/autocomplete_light/jquery.init.js
+++ src/dal/static/autocomplete_light/jquery.init.js
@@ -6,15 +6,4 @@ var yl = yl || {};
 // namespace and restore the original values of window.$ and window.jQuery.
 // This is necessary so that code which uses a version of jQuery that may
 // have been installed up-page gets the version it expects.
-yl.jQuery = jQuery.noConflict(true);
-
-// Here, we set up django.jQuery, instead of letting admin/js/jquery.init.js
-// do it. This is necessary because it ALSO calls jQuery.noConflict(true).
-// It we let it do that, it would break any code further down the page that
-// expects window.$ and window.jQuery to be defined.
-var django = django || {};
-if (typeof django.jQuery === 'undefined') {
-    // If django.jQuery is not yet defined, we need to define it so that
-    // admin/js/autocomplete.js (which we include with our widget) can run.
-    django.jQuery = yl.jQuery;
-}
+yl.jQuery = django.jQuery.noConflict(true);
